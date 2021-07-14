156502168dd58d66d2b9ed077faa6dbbcdc14f5e
lambda <jrigada@frba.utn.edu.ar>
2021-07-14 12:14:01 -0300

    change hostname in dev.ex for local use

--- config/dev.exs
+++ config/dev.exs
@@ -5,7 +5,7 @@ config :chat, Chat.Repo,
   username: "postgres",
   password: "postgres",
   database: "chat_dev",
-  hostname: "db",
+  hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
